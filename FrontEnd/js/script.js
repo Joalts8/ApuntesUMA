87d7b7726325dbf692e86589dd0e5638365a158b
Joalts8 <jorgenexus8@gmail.com>
2026-03-08 20:00:43 +0100

    js

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,3 +1,10 @@
-let variable = 10
+let variable = 10;               // declaracion de variable y asignacion de valor
+let tipo = typeof variable;      // tipo de dato de la variable
+//tipos de datos: number, string, boolean, object, undefined, null, operadores aritmeticos: +, -, *, /, % (modulo), 
+// operadores de asignacion: =, +=, -=, *=, /=, ++, -- operadores de comparacion: ==, ===, !=, !==, >, <, >=, <=
 
-console.log(variable, typeof variable)
+console.log(variable, `del tipo ${tipo}`);     //muestra por consola. Uso de tilde invertida para mostrar con ${}
+
+window.alert(variable)                        //muestra por ventana emergente(alerta)
+
+document.getElementById("H1").textContent = variable;    //en el elemento con la id, muestra el valor
